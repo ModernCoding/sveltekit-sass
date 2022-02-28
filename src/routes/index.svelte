a38b5bb82a210f77d48690b56d4bb6c913c5532e
Cormorant Programming <cormorant.programming@gmail.com>
2022-02-28 20:19:52 +0700

    sass

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="box">
+  <h1 class="title">
+    Hello World
+  </h1>
+</div>
+
+
+<style type="text/css" lang="sass">
+
+  @use "sass:math"
+
+  :global(body)
+    margin: 0
+    display: flex
+    align-items: center
+    justify-content: center
+    min-height: 100vh
+
+  :global(h1)
+    margin: 0
+
+  $phi: (2 * math.sin(54deg))
+  
+  .box
+    background: red
+    height: calc(100vh / $phi)
+    width: calc(100% / $phi)
+    display: flex
+    align-items: center
+    justify-content: center
+
+    > h1
+      color: white
+
+</style>
